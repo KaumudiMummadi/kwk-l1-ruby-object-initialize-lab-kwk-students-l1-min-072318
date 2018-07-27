dd52263610cb0a6c446c1ac142b943a29ea783f2
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-27 21:01:05 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,17 +3,5 @@ class Person
   def initialize(name)
     @name = name
   end 
- 
-
-  def name= (name)
-   @name = name
-  end 
-
-  def name 
-  @name 
 end 
-end 
-
-
-beyonce= Person.new("Beyonce")
   
