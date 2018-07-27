5feef083aedd1c0d9941b06bc77b467aeb0606f5
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-27 20:55:45 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,16 +3,16 @@ class Person
   def initialize(name)
     @name=name
   end 
+ 
+
+  def name= (name)
+  @name=name
+  end 
+
+  def name 
+  @name 
 end 
-
-#   def name= (name)
-#   @name=name
-#   end 
-
-#   def name 
-#   @name 
-# end 
-
+end 
 
 
 beyonce= Person.new("Beyonce")
